1e34008a5515429ed8d0f934a0d39871b96b0d7a
SergeyB <sergiibugaienko@gmail.com>
2024-10-29 19:56:46 +0100

    add test C

---
+++ src/Task2.java
@@ -0,0 +1,11 @@
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 29.10.2024
+ */
+
+public class Task2 {
+
+    public static void testC() {
+        System.out.println("Hello from user 2");
+    }
+}
