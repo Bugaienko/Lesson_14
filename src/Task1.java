ff958e8aee7a2afcf9068758c2b713fd06e90b92
SergeyB <sergiibugaienko@gmail.com>
2024-10-29 19:03:22 +0100

    add testB()

--- src/Task1.java
+++ src/Task1.java
@@ -8,4 +8,8 @@ public class Task1 {
     public static void testA() {
         System.out.println("Hello World");
     }
+
+    public static void testB() {
+        System.out.println("Test B");
+    }
 }
