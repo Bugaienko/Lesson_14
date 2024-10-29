e887da23dd87c398d165a29b1638b5326d628d00
SergeyB <sergiibugaienko@gmail.com>
2024-10-29 20:04:55 +0100

    add main

---
+++ src/Task3.java
@@ -0,0 +1,11 @@
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 29.10.2024
+ */
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        System.out.println("Main!");
+    }
+}
